b42bcc9c3c59c390c84e314c58e32625f76f6b1c
Sadio Ali <sadiali@pdx.edu>
2016-05-24 08:41:41 -0700

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -48,6 +48,7 @@ gulp.task('serve', function() {
   gulp.watch(['js/*.js'], ['jsBuild']);
   gulp.watch(['bower.json'], ['bowerBuild']);
   gulp.watch(["scss/*.scss"], ['cssBuild']);
+  gulp.watch(["/*.html"], ['htmlBuild']);
 });
 
 gulp.task('jsBuild', ['jsBrowserify', 'jshint'], function(){
@@ -58,10 +59,23 @@ gulp.task('bowerBuild', ['bower'], function(){
   browserSync.reload();
 });
 
+gulp.task('htmlBuild', function(){
+  browserSync.reload();
+});
+
+gulp.task('styleBuild', function(){
+  browserSync.reload();
+});
+
 //////// WATCH TASKS //////////
 gulp.task('watch', function(){
   gulp.watch('js/**/*.js', ['scripts']);
+  gulp.watch(['js/*.js'], ['jsBuild']);
+  gulp.watch(['bower.json'], ['bowerBuild']);
+  gulp.watch(["scss/*.scss"], ['cssBuild']);
+  gulp.watch(["css/*.css"], ['styleBuild']);
+  gulp.watch(["*.html"], ['htmlBuild']);
 });
 
 //////// DEFAULT TASKS //////////
-gulp.task('default', ['scripts', 'watch', 'jshint']);
+gulp.task('default', ['scripts', 'watch', 'jshint', 'serve']);
