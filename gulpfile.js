60736c61389abcd705d625fe80607abe0d897cf4
sadiyah <sadiyah-ali@hotmail.com>
2016-05-23 18:36:50 -0700

    Add Jshint

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     compass = require('gulp-compass'),
+    jshint = require('gulp-jshint'),
     browserSync = require('browser-sync').create(),
     sass = require('gulp-sass'),
-    sourcemaps = require('gulp-sourcemaps');
+    sourcemaps = require('gulp-sourcemaps'),
+    plumber = require('gulp-plumber'); //Keeps watch going even if you make a syntax error
 
 //////// SCRIPTS TASKS //////////
 gulp.task('scripts', function(){
@@ -12,9 +14,17 @@ gulp.task('scripts', function(){
   .pipe(gulp.dest('app/js'));
 });
 
+//////// JsHint //////////
+gulp.task('jshint', function(){
+  return gulp.src(['js/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
 //////// CSSBUILD //////////
 gulp.task('cssBuild', function() {
   return gulp.src('scss/*.scss')
+    .pipe(plumber())
     .pipe(sourcemaps.init())
     .pipe(sass())
     .pipe(sourcemaps.write())
@@ -49,4 +59,4 @@ gulp.task('watch', function(){
 
 
 //////// DEFAULT TASKS //////////
-gulp.task('default', ['scripts', 'watch']);
+gulp.task('default', ['scripts', 'watch', 'jshint']);
