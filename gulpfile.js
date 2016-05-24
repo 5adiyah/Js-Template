376dfaeba44abc2561dcd013a0792f8adf9209e8
sadiyah <sadiyah-ali@hotmail.com>
2016-05-23 18:28:50 -0700

    Add sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,52 @@
 var gulp = require('gulp'),
-    uglify = require('gulp-uglify');
+    uglify = require('gulp-uglify'),
+    compass = require('gulp-compass'),
+    browserSync = require('browser-sync').create(),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps');
 
 //////// SCRIPTS TASKS //////////
 gulp.task('scripts', function(){
-  gulp.src('js/*.js') //Grabs all js files, uglifies it, and pipes to destination
+  gulp.src('js/**/*.js') //Grabs all js files, uglifies it, and pipes to destination
   .pipe(uglify())
   .pipe(gulp.dest('app/js'));
 });
 
+//////// CSSBUILD //////////
+gulp.task('cssBuild', function() {
+  return gulp.src('scss/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./build/css'));
+});
+
+//////// BrowserSync //////////
+gulp.task('serve', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./",
+      index: "index.html"
+    }
+  });
+
+  gulp.watch(['js/*.js'], ['jsBuild']);
+  gulp.watch(['bower.json'], ['bowerBuild']);
+  gulp.watch(["scss/*.scss"], ['cssBuild']);
+});
+
+gulp.task('jsBuild', ['jsBrowserify', 'jshint'], function(){
+  browserSync.reload();
+});
+
+gulp.task('bowerBuild', ['bower'], function(){
+  browserSync.reload();
+});
+//////// WATCH TASKS //////////
+gulp.task('watch', function(){
+  gulp.watch('js/**/*.js', ['scripts']);
+});
 
 
 //////// DEFAULT TASKS //////////
-gulp.task('default', ['scripts']);
+gulp.task('default', ['scripts', 'watch']);
